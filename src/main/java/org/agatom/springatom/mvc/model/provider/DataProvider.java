f52549ac2685ca7e3b7059b86657f047d0277d41
kornicameister <kornicameister@gmail.com>
2013-05-13 02:22:31 +0200

    #2 generic DAO introduced

--- src/main/java/org/agatom/springatom/mvc/model/provider/DataProvider.java
+++ src/main/java/org/agatom/springatom/mvc/model/provider/DataProvider.java
@@ -1,37 +1,17 @@
 package org.agatom.springatom.mvc.model.provider;
 
-import org.apache.log4j.Logger;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.agatom.springatom.model.Persistable;
 
-import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
 
+/**
+ * @author kornicamaister
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public interface DataProvider<T extends Persistable> {
+    T getById(@NotNull final Long id);
 
-@Repository(value = "DaoSupport")
-abstract public class DataProvider {
-    private static final Logger LOGGER = Logger.getLogger(DataProvider.class);
-
-    @Resource
-    SessionFactory sessionFactory;
-
-    @Autowired(required = true)
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-        if (LOGGER.isTraceEnabled()) {
-            LOGGER.trace(String.format("Initialized sessionFactory=%s", this.sessionFactory));
-        }
-    }
-
-    public Session getSession() {
-        if (LOGGER.isTraceEnabled()) {
-            LOGGER.trace(String.format("Getting session factory, sessionFactory=%s", this.sessionFactory));
-        }
-        return this.sessionFactory.getCurrentSession();
-    }
-
-    public SessionFactory getSessionFactory() {
-        return this.sessionFactory;
-    }
+    Set<T> getAll();
 }
--- src/main/java/org/agatom/springatom/mvc/model/provider/impl/SMetaDataProviderImpl.java
+++ src/main/java/org/agatom/springatom/mvc/model/provider/impl/SMetaDataProviderImpl.java
@@ -1,63 +1,39 @@
 package org.agatom.springatom.mvc.model.provider.impl;
 
 import org.agatom.springatom.model.meta.SMetaData;
-import org.agatom.springatom.mvc.model.provider.DataProvider;
+import org.agatom.springatom.mvc.model.provider.DefaultDataProvider;
 import org.agatom.springatom.mvc.model.provider.SMetaDataProvider;
-import org.apache.log4j.Logger;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.constraints.NotNull;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
-import static org.agatom.springatom.util.QueryConstants.Error.INACTIVE_SESSION;
-import static org.agatom.springatom.util.QueryConstants.QueryBody.FROM_TABLE_BY_TABLE_NAME;
-import static org.agatom.springatom.util.QueryConstants.QueryResult.FROM_TABLE_RESULT_WITH_CLASS_NAME;
-import static org.agatom.springatom.util.QueryConstants.QueryTrace.READ_ENTITY_FROM_TABLE;
 
 /**
  * @author kornicameister
  * @version 0.0.1
  * @since 0.0.1
  */
+@SuppressWarnings("SpringElInspection")
 @Repository(value = "STypeDataDao")
 @Transactional(readOnly = true, propagation = Propagation.REQUIRED)
-public class SMetaDataProviderImpl extends DataProvider implements SMetaDataProvider {
-    private static final Logger LOGGER = Logger.getLogger(SMetaDataProviderImpl.class);
-
+public class SMetaDataProviderImpl extends DefaultDataProvider<SMetaData> implements SMetaDataProvider {
     private static final String TYPE = "type";
 
     @Override
     @Cacheable(value = "typedata")
     public Set<SMetaData> getAll(@NotNull final Class<? extends SMetaData> clazz) {
-        if (this.getSession() == null) {
-            LOGGER.warn(INACTIVE_SESSION);
-            return null;
-        }
-        List<?> dataList = this.getSession()
-                .createQuery(String.format(FROM_TABLE_BY_TABLE_NAME, clazz.getSimpleName()))
-                .list();
-        Set<SMetaData> dataSet = new HashSet<>();
-        for (Object entity : dataList) {
-            if (LOGGER.isTraceEnabled()) {
-                LOGGER.trace(String.format(READ_ENTITY_FROM_TABLE, entity, clazz.getSimpleName()));
-            }
-            dataSet.add((SMetaData) entity);
-        }
-        if (LOGGER.isTraceEnabled()) {
-            LOGGER.trace(String.format(FROM_TABLE_RESULT_WITH_CLASS_NAME, dataSet.size(), clazz.getName()));
-        }
-        return dataSet;
+        this.setTarget(clazz);
+        return this.getAll();
     }
 
     @Override
     @Cacheable(value = "typedata", key = "#id")
     public SMetaData getById(@NotNull final Long id, @NotNull final Class<? extends SMetaData> clazz) {
-        return (SMetaData) this.getSession().load(clazz, id);
+        this.setTarget(clazz);
+        return this.getById(id);
     }
 
     @Override
