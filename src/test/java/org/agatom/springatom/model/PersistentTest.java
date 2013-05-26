34eb20dd947b5e7480f28cc835903857114cd90c
kornicameister <kornicameister@gmail.com>
2013-05-26 13:22:07 +0200

    for further development, updated copyrights

--- src/test/java/org/agatom/springatom/model/PersistentTest.java
+++ src/test/java/org/agatom/springatom/model/PersistentTest.java
@@ -1,3 +1,20 @@
+/**************************************************************************************************
+ * This file is part of [SpringAtom] Copyright [kornicameister@gmail.com][2013]                   *
+ *                                                                                                *
+ * [SpringAtom] is free software: you can redistribute it and/or modify                           *
+ * it under the terms of the GNU General Public License as published by                           *
+ * the Free Software Foundation, either version 3 of the License, or                              *
+ * (at your option) any later version.                                                            *
+ *                                                                                                *
+ * [SpringAtom] is distributed in the hope that it will be useful,                                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  *
+ * GNU General Public License for more details.                                                   *
+ *                                                                                                *
+ * You should have received a copy of the GNU General Public License                              *
+ * along with [SpringAtom].  If not, see <http://www.gnu.org/licenses/gpl.html>.                  *
+ **************************************************************************************************/
+
 package org.agatom.springatom.model;
 
 import org.agatom.springatom.model.client.SClient;
@@ -44,6 +61,15 @@ public class PersistentTest {
         for (Class aClass : this.clazzList) {
             Persistable persistable = (Persistable) aClass.newInstance();
             Assert.assertEquals(aClass.getSimpleName(), persistable.getEntityName());
+        }
+    }
+
+    @Test
+    public void testIdColumnName() throws Exception {
+        for (Class aClass : this.clazzList) {
+            Persistable persistable = (Persistable) aClass.newInstance();
+            System.out.println(String.format("%s = %s", aClass.getSimpleName(), persistable.getIdColumnName()));
+//            Assert.assertEquals("id" + aClass.getSimpleName(), persistable.getIdColumnName());
         }
     }
 }
