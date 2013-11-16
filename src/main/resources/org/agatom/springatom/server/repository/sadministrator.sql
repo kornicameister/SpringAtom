5ceaedac53d5c5a23aae27b71859b447aed8688d
kornicameister <kornicameister@gmail.com>
2013-11-16 18:41:58 +0100

    SpringAtom-#48
    updated sadministrator.sql

--- src/main/resources/org/agatom/springatom/server/repository/sadministrator.sql
+++ src/main/resources/org/agatom/springatom/server/repository/sadministrator.sql
@@ -1,30 +1,10 @@
-INSERT INTO `suser` (idSUser,
-                     createdDate,
-                     lastModifiedDate,
-                     username,
-                     password,
-                     accountNonExpired,
-                     accountNonLocked,
-                     credentialsNonExpired,
-                     enabled,
-                     createdBy_idSUser,
-                     lastModifiedBy_idSUser,
-                     person,
-                     version)
+INSERT INTO `sperson` (idSPerson, createdDate, lastModifiedDate, version, primaryMail, fName, lName, createdBy_idSUser, lastModifiedBy_idSUser)
+VALUES (1, '2013-11-13 00:00:00', '2013-11-13 00:00:00', '1', 'kornicameister@gmail.com', 'Tomasz', 'Trębski', NULL, NULL);
+
+INSERT INTO `suser` (idSUser, createdDate, lastModifiedDate, username, password, accountNonExpired, accountNonLocked, credentialsNonExpired, enabled, createdBy_idSUser, lastModifiedBy_idSUser, person, version)
 VALUES
-  (1,
-   null,
-   null,
-   "SYSTEM",
-   "SYSTEM",
-   TRUE,
-   TRUE,
-   TRUE,
-   TRUE,
-   null,
-   null,
-   null,
-   0);
+  (1, '2013-11-13 00:00:00', '2013-11-13 00:00:00', "SYSTEM", "$2a$10$b3Xjq0/iV31I911hIpuVKuBR19iN2YSDtj6crlZhOkScb7hFmoDJy", TRUE, TRUE, TRUE, TRUE,
+   null, null, null, 0);
 
 INSERT INTO `suserauthority` (user, authority) VALUES (1, (SELECT
                                                              `idSAuthority`
