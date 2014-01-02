aa1bc4e7d2df898e2f57aef05b39828f98d57902
kornicameister <kornicameister@gmail.com>
2014-01-02 18:40:32 +0100

    SpringAtom - #86
    Wizards:
    - WizardEvents boosted with ability to check whether or not passed event name is associated with any WizardEvent
    - WizardDynamicEvent wraps custom WizardEvent (or any of fixed events)

---
+++ src/main/java/org/agatom/springatom/webmvc/flows/wizard/AbstractWizard.java
@@ -0,0 +1,57 @@
+/**************************************************************************************************
+ * This file is part of [SpringAtom] Copyright [kornicameister@gmail.com][2014]                   *
+ *                                                                                                *
+ * [SpringAtom] is free software: you can redistribute it and/or modify                           *
+ * it under the terms of the GNU General Public License as published by                           *
+ * the Free Software Foundation, either version 3 of the License, or                              *
+ * (at your option) any later version.                                                            *
+ *                                                                                                *
+ * [SpringAtom] is distributed in the hope that it will be useful,                                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  *
+ * GNU General Public License for more details.                                                   *
+ *                                                                                                *
+ * You should have received a copy of the GNU General Public License                              *
+ * along with [SpringAtom].  If not, see <http://www.gnu.org/licenses/gpl.html>.                  *
+ **************************************************************************************************/
+
+package org.agatom.springatom.webmvc.flows.wizard;
+
+import com.google.common.base.Objects;
+import org.agatom.springatom.server.model.beans.user.SUser;
+import org.agatom.springatom.web.beans.WebBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.webflow.execution.RequestContext;
+
+/**
+ * @author kornicameister
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+abstract public class AbstractWizard
+        implements WebBean,
+                   InitializingBean {
+    private static final String DEFAULT_TITLE = "NewWizard";
+    protected String title;
+    protected SUser  createdBy;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public SUser getCreatedBy() {
+        return createdBy;
+    }
+
+    public void init(final RequestContext context) {
+        this.title = context.getRequestParameters().get("title", AbstractWizard.DEFAULT_TITLE);
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                      .addValue(title)
+                      .addValue(createdBy)
+                      .toString();
+    }
+}
--- src/main/java/org/agatom/springatom/webmvc/flows/wizard/events/WizardEvents.java
+++ src/main/java/org/agatom/springatom/webmvc/flows/wizard/events/WizardEvents.java
@@ -17,10 +17,17 @@
 
 package org.agatom.springatom.webmvc.flows.wizard.events;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * @author kornicameister
@@ -31,23 +38,51 @@ import org.springframework.stereotype.Component;
 @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public final class WizardEvents {
 
+    private static final String       NEXT_EVENT     = "next";
+    private static final String       PREVIOUS_EVENT = "previous";
+    private static final String       CANCEL_EVENT   = "cancel";
+    private static final String       FINISH_EVENT   = "finish";
+    private static final List<String> EVENTS         = Lists.newArrayList(NEXT_EVENT, PREVIOUS_EVENT, CANCEL_EVENT, FINISH_EVENT);
+
+    public boolean isWizardEvent(final String eventId) {
+        if (!StringUtils.hasText(eventId)) {
+            return false;
+        }
+        final String anotherString = StringUtils.trimAllWhitespace(eventId);
+        return FluentIterable
+                .from(EVENTS)
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean apply(@Nullable final String input) {
+                        assert input != null;
+                        return input.equalsIgnoreCase(anotherString);
+                    }
+                })
+                .first()
+                .isPresent();
+    }
+
     @Bean(name = "WizardNextEvent")
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public WizardEvent getNext() {
-        return new WizardEvent().init("next");
+        return new WizardEvent().init(NEXT_EVENT);
     }
 
     @Bean(name = "WizardPreviousEvent")
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public WizardEvent getPrevious() {
-        return new WizardEvent().init("previous");
+        return new WizardEvent().init(PREVIOUS_EVENT);
     }
 
     @Bean(name = "WizardCancelEvent")
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public WizardEvent getCancel() {
-        return new WizardEvent().init("cancel");
+        return new WizardEvent().init(CANCEL_EVENT);
     }
 
     @Bean(name = "WizardFinishEvent")
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public WizardEvent getFinish() {
-        return new WizardEvent().init("finish");
+        return new WizardEvent().init(FINISH_EVENT);
     }
 }
