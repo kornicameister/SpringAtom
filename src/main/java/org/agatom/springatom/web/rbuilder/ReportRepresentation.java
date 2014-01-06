cfe48ec240c8ad42c00fce8e3dc40d5aec94432d
kornicameister <kornicameister@gmail.com>
2014-01-06 14:31:04 +0100

    SpringAtom - #92
    ReportRepresentation.java is a bean (out of the Spring's context) that loads all available representations (by checking if subclasses of AbstractJasperReportView are available):
    for CSV it is JasperReportsCsvView
    for HTML it is JasperReportsHtmlView
    for PDF it is JasperReportsPdfView
    for EXCEL it is JasperReportsXlsView

--- src/main/java/org/agatom/springatom/web/rbuilder/ReportRepresentation.java
+++ src/main/java/org/agatom/springatom/web/rbuilder/ReportRepresentation.java
@@ -17,10 +17,99 @@
 
 package org.agatom.springatom.web.rbuilder;
 
+import com.google.common.base.Objects;
+import com.google.common.collect.Maps;
+import org.springframework.hateoas.Identifiable;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsCsvView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsHtmlView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsPdfView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsXlsView;
+
+import javax.annotation.Nonnull;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Map;
+
 /**
  * @author kornicameister
  * @version 0.0.1
  * @since 0.0.1
  */
-public class ReportRepresentation {
+public class ReportRepresentation
+        implements Serializable,
+                   Comparable<ReportRepresentation> {
+
+    private static Map<String, ReportRepresentation> REP = null;
+    private final String type;
+    private final String key;
+
+    private ReportRepresentation(final Representation representation) {
+        this.key = representation.getId();
+        this.type = representation.name().toLowerCase();
+    }
+
+    public static Map<String, ReportRepresentation> getRepresentation() {
+        if (REP == null) {
+            final Map<String, ReportRepresentation> map = Maps.newHashMap();
+            for (final Representation representation : Representation.values()) {
+                if (representation.isPresent()) {
+                    map.put(representation.getId(),
+                            new ReportRepresentation(representation)
+                    );
+                }
+            }
+            REP = Collections.unmodifiableMap(map);
+        }
+        return REP;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this)
+                      .addValue(type)
+                      .addValue(key)
+                      .toString();
+    }
+
+    @Override
+    public int compareTo(@Nonnull final ReportRepresentation o) {
+        return this.key.compareTo(o.key);
+    }
+
+    public static enum Representation
+            implements Serializable,
+                       Identifiable<String> {
+        CSV(JasperReportsCsvView.class, "csv"),
+        HTML(JasperReportsHtmlView.class, "html"),
+        PDF(JasperReportsPdfView.class, "pdf"),
+        EXCEL(JasperReportsXlsView.class, "xls");
+
+        private final boolean present;
+        private final String  mappingKey;
+
+        Representation(final Class<?> exporter, final String mappingKey) {
+            this.present = ClassUtils.isPresent(exporter.getName(), ReportRepresentation.class.getClassLoader());
+            this.mappingKey = mappingKey;
+        }
+
+        public boolean isPresent() {
+            return present;
+        }
+
+        @Override
+        public String getId() {
+            return this.mappingKey;
+        }
+    }
+
 }
