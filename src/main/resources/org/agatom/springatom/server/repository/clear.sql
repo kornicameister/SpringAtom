d83f173e5e01a7772ba7d118ad355b491c797093
kornicameister <kornicameister@gmail.com>
2014-03-03 18:34:05 +0100

    Updated after migration to Linux

--- src/main/resources/org/agatom/springatom/server/repository/clear.sql
+++ src/main/resources/org/agatom/springatom/server/repository/clear.sql
@@ -1,19 +1,18 @@
 SET foreign_key_checks = 0;
 
-TRUNCATE TABLE `sauthority`;
-TRUNCATE TABLE `suser`;
-TRUNCATE TABLE `suser_history`;
-TRUNCATE TABLE `sperson`;
-TRUNCATE TABLE `sperson_history`;
+TRUNCATE TABLE `SAuthority`;
+TRUNCATE TABLE `SUser`;
+TRUNCATE TABLE `SPerson_history`;
+TRUNCATE TABLE `SPerson`;
 TRUNCATE TABLE `contacts`;
 TRUNCATE TABLE `contacts_history`;
-TRUNCATE TABLE `suserauthority`;
+TRUNCATE TABLE `SUserAuthority`;
 TRUNCATE TABLE `revinfo`;
 TRUNCATE TABLE `appointment`;
 TRUNCATE TABLE `appointment_task`;
 TRUNCATE TABLE `cars`;
 TRUNCATE TABLE `cars_history`;
-TRUNCATE TABLE `revchanges`;
+TRUNCATE TABLE `REVCHANGES`;
 TRUNCATE TABLE `car_master`;
 TRUNCATE TABLE `notifications`;
 TRUNCATE TABLE `acl_class`;
@@ -21,7 +20,6 @@ TRUNCATE TABLE `acl_object_identity`;
 TRUNCATE TABLE `acl_entry`;
 TRUNCATE TABLE `acl_sid`;
 TRUNCATE TABLE `issues`;
-TRUNCATE TABLE `revchanges`;
 DROP TABLE IF EXISTS `persistent_logins`;
 
 SET foreign_key_checks = 1;
--- src/main/resources/org/agatom/springatom/server/repository/sadministrator.sql
+++ src/main/resources/org/agatom/springatom/server/repository/sadministrator.sql
@@ -1,22 +1,24 @@
-INSERT INTO `sperson` (idSPerson, createdDate, lastModifiedDate, version, primaryMail, fName, lName, createdBy_idSUser, lastModifiedBy_idSUser)
-VALUES (1, '2013-11-13 00:00:00', '2013-11-13 00:00:00', '1', 'kornicameister@gmail.com', 'Tomasz', 'Trębski', NULL, NULL);
-
-INSERT INTO `suser` (idSUser, createdDate, lastModifiedDate, username, password, accountNonExpired, accountNonLocked, credentialsNonExpired, enabled, createdBy_idSUser, lastModifiedBy_idSUser, person, version)
+INSERT INTO `SPerson` (idSPerson, createdDate, lastModifiedDate, version, primaryMail, fName, lName, createdBy_idSUser, lastModifiedBy_idSUser)
 VALUES
-  (1, '2013-11-13 00:00:00', '2013-11-13 00:00:00', "SYSTEM", "$2a$10$b3Xjq0/iV31I911hIpuVKuBR19iN2YSDtj6crlZhOkScb7hFmoDJy", TRUE, TRUE, TRUE, TRUE,
-   null, null, null, 0);
+  (1, '2013-11-13 00:00:00', '2013-11-13 00:00:00', '1', 'kornicameister@gmail.com', 'Tomasz', 'Trębski', NULL, NULL);
 
-INSERT INTO `suserauthority` (user, authority) VALUES (1, (SELECT
+INSERT INTO `SUser` (idSUser, createdDate, lastModifiedDate, username, password, accountNonExpired, accountNonLocked, credentialsNonExpired, enabled, createdBy_idSUser, lastModifiedBy_idSUser, person, version)
+VALUES
+  (1, '2013-11-13 00:00:00', '2013-11-13 00:00:00', "SYSTEM",
+   "$2a$10$b3Xjq0/iV31I911hIpuVKuBR19iN2YSDtj6crlZhOkScb7hFmoDJy", TRUE, TRUE, TRUE, TRUE,
+   NULL, NULL, NULL, 0);
+
+INSERT INTO `SUserAuthority` (user, authority) VALUES (1, (SELECT
                                                              `idSAuthority`
-                                                           FROM `sauthority`
+                                                           FROM `SAuthority`
                                                            WHERE `authority` = "ROLE_ADMIN"));
 
-INSERT INTO `suserauthority` (user, authority) VALUES (1, (SELECT
+INSERT INTO `SUserAuthority` (user, authority) VALUES (1, (SELECT
                                                              `idSAuthority`
-                                                           FROM `sauthority`
+                                                           FROM `SAuthority`
                                                            WHERE `authority` = "ROLE_CLIENT"));
 
-INSERT INTO `suserauthority` (user, authority) VALUES (1, (SELECT
+INSERT INTO `SUserAuthority` (user, authority) VALUES (1, (SELECT
                                                              `idSAuthority`
-                                                           FROM `sauthority`
+                                                           FROM `SAuthority`
                                                            WHERE `authority` = "ROLE_MECHANIC"));
