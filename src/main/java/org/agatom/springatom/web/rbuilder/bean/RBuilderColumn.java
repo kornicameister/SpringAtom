3fb3c7b550f83286f9a58148515a09051bba623c
kornicameister <kornicameister@gmail.com>
2014-03-20 01:03:22 +0100

    updated pom.xml + other files

--- src/main/java/org/agatom/springatom/web/rbuilder/bean/RBuilderColumn.java
+++ src/main/java/org/agatom/springatom/web/rbuilder/bean/RBuilderColumn.java
@@ -18,8 +18,8 @@
 package org.agatom.springatom.web.rbuilder.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.common.base.Objects;
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.springframework.util.ClassUtils;
 
 import java.util.Map;
@@ -31,150 +31,150 @@ import java.util.Map;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RBuilderColumn
-        extends RBuilderBean {
-    public static final int     DEFAULT_ORDER = -1;
-    private static final long                  serialVersionUID = 2600080347152145806L;
-    private static final Class<String>         DEFAULT_RENDER   = String.class;
-    protected           Integer order         = DEFAULT_ORDER;
-    protected            String                prefix           = null;
-    protected            String                columnName       = null;
-    protected            Class<?>              columnClass      = Void.class;
-    protected            Class<?>              renderClass      = DEFAULT_RENDER;
-    protected            RBuilderColumnOptions options          = null;
-    protected            Class<?>              elementClass     = null;
+		extends RBuilderBean {
+	public static final  int                   DEFAULT_ORDER    = -1;
+	private static final long                  serialVersionUID = 2600080347152145806L;
+	private static final Class<String>         DEFAULT_RENDER   = String.class;
+	protected            Integer               order            = DEFAULT_ORDER;
+	protected            String                prefix           = null;
+	protected            String                columnName       = null;
+	protected            Class<?>              columnClass      = Void.class;
+	protected            Class<?>              renderClass      = DEFAULT_RENDER;
+	protected            RBuilderColumnOptions options          = null;
+	protected            Class<?>              elementClass     = null;
 
-    public RBuilderColumn setOrder(final Integer order) {
-        this.order = order;
-        return this;
-    }
+	public RBuilderColumn setOrder(final Integer order) {
+		this.order = order;
+		return this;
+	}
 
-    public Integer getOrder() {
-        return order;
-    }
+	public Integer getOrder() {
+		return order;
+	}
 
-    public RBuilderColumn setOptions(final RBuilderColumnOptions options) {
-        this.options = options;
-        return this;
-    }
+	public RBuilderColumn setOptions(final RBuilderColumnOptions options) {
+		this.options = options;
+		return this;
+	}
 
-    public RBuilderColumnOptions getOptions() {
-        if (this.options == null) {
-            this.options = new RBuilderColumnOptions();
-        }
-        return options;
-    }
+	public RBuilderColumnOptions getOptions() {
+		if (this.options == null) {
+			this.options = new RBuilderColumnOptions();
+		}
+		return options;
+	}
 
-    public RBuilderColumn setPrefix(final String prefix) {
-        this.prefix = prefix;
-        return this;
-    }
+	public RBuilderColumn setPrefix(final String prefix) {
+		this.prefix = prefix;
+		return this;
+	}
 
-    public String getPrefix() {
-        return prefix;
-    }
+	public String getPrefix() {
+		return prefix;
+	}
 
-    public RBuilderColumn setColumnClass(final Class<?> columnClass) {
-        this.columnClass = columnClass;
-        return this;
-    }
+	public RBuilderColumn setColumnClass(final Class<?> columnClass) {
+		this.columnClass = columnClass;
+		return this;
+	}
 
-    public Class<?> getColumnClass() {
-        return columnClass;
-    }
+	public Class<?> getColumnClass() {
+		return columnClass;
+	}
 
-    public String getColumnName() {
-        return columnName;
-    }
+	public String getColumnName() {
+		return columnName;
+	}
 
-    public RBuilderColumn setColumnName(final String columnName) {
-        this.columnName = columnName;
-        return this;
-    }
+	public RBuilderColumn setColumnName(final String columnName) {
+		this.columnName = columnName;
+		return this;
+	}
 
-    public Class<?> getRenderClass() {
-        if (this.renderClass == null) {
-            this.renderClass = String.class;
-        }
-        return this.renderClass;
-    }
+	public Class<?> getRenderClass() {
+		if (this.renderClass == null) {
+			this.renderClass = String.class;
+		}
+		return this.renderClass;
+	}
 
-    public void setRenderClass(final Class<?> renderClass) {
-        this.renderClass = renderClass;
-    }
+	public void setRenderClass(final Class<?> renderClass) {
+		this.renderClass = renderClass;
+	}
 
-    public RBuilderColumn setElementClass(final Class<?> elementClass) {
-        this.elementClass = elementClass;
-        return this;
-    }
+	public RBuilderColumn setElementClass(final Class<?> elementClass) {
+		this.elementClass = elementClass;
+		return this;
+	}
 
-    public Class<?> getElementClass() {
-        return elementClass;
-    }
+	public Class<?> getElementClass() {
+		return elementClass;
+	}
 
-    /**
-     * Evaluates if this column most likely represents single-valued column.
-     *
-     * @return true if so, false otherwise
-     *
-     * @see RBuilderColumn#isMultiValued()
-     */
+	/**
+	 * Evaluates if this column most likely represents single-valued column.
+	 *
+	 * @return true if so, false otherwise
+	 *
+	 * @see RBuilderColumn#isMultiValued()
+	 */
 
-    @JsonIgnore
-    public boolean isSingleValued() {
-        return !this.isMultiValued();
-    }
+	@JsonIgnore
+	public boolean isSingleValued() {
+		return !this.isMultiValued();
+	}
 
-    /**
-     * Evaluates is this columns most likely represents multi valued column.
-     * Method checks if that's so by verifying if {@link RBuilderColumn#columnClass}
-     * is assignable either from {@link java.lang.Iterable} or {@link java.util.Map}
-     *
-     * @return true if multi valued
-     */
+	/**
+	 * Evaluates is this columns most likely represents multi valued column.
+	 * Method checks if that's so by verifying if {@link RBuilderColumn#columnClass}
+	 * is assignable either from {@link java.lang.Iterable} or {@link java.util.Map}
+	 *
+	 * @return true if multi valued
+	 */
 
-    @JsonIgnore
-    public boolean isMultiValued() {
-        return ClassUtils.isAssignable(Iterable.class, this.columnClass)
-                || ClassUtils.isAssignable(Map.class, this.columnClass);
-    }
+	@JsonIgnore
+	public boolean isMultiValued() {
+		return ClassUtils.isAssignable(Iterable.class, this.columnClass)
+				|| ClassUtils.isAssignable(Map.class, this.columnClass);
+	}
 
-    @Override
-    @JsonIgnore
-    public String getMessageKey() {
-        return String.format("%s.%s", this.prefix, this.columnName);
-    }
+	@Override
+	@JsonIgnore
+	public String getMessageKey() {
+		return String.format("%s.%s", this.prefix, this.columnName);
+	}
 
 
-    @Override
-    protected Integer calculateId() {
-        return this.hashCode();
-    }
+	@Override
+	protected Integer calculateId() {
+		return this.hashCode();
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
-        RBuilderColumn that = (RBuilderColumn) o;
+		RBuilderColumn that = (RBuilderColumn) o;
 
-        return Objects.equal(this.order, that.order) &&
-                Objects.equal(this.prefix, that.prefix) &&
-                Objects.equal(this.columnName, that.columnName) &&
-                Objects.equal(this.columnClass, that.columnClass) &&
-                Objects.equal(this.renderClass, that.renderClass) &&
-                Objects.equal(this.options, that.options) &&
-                Objects.equal(this.elementClass, that.elementClass) &&
-                Objects.equal(this.label, that.label) &&
-                Objects.equal(this.id, that.id);
-    }
+		return Objects.equal(this.order, that.order) &&
+				Objects.equal(this.prefix, that.prefix) &&
+				Objects.equal(this.columnName, that.columnName) &&
+				Objects.equal(this.columnClass, that.columnClass) &&
+				Objects.equal(this.renderClass, that.renderClass) &&
+				Objects.equal(this.options, that.options) &&
+				Objects.equal(this.elementClass, that.elementClass) &&
+				Objects.equal(this.label, that.label) &&
+				Objects.equal(this.id, that.id);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(order, prefix, columnName, columnClass, renderClass, options,
-                elementClass, label, id);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(order, prefix, columnName, columnClass, renderClass, options,
+				elementClass, label, id);
+	}
 }
