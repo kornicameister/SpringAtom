a5f4fd0ecb18a6c6810eddd0545bf324e4a85bfb
kornicameister <kornicameister@gmail.com>
2014-03-21 02:45:04 +0100

    SpringAtom - #67
    Little optimization and configuration

--- src/main/java/org/agatom/springatom/rest/SARepositoryRestMvcConfiguration.java
+++ src/main/java/org/agatom/springatom/rest/SARepositoryRestMvcConfiguration.java
@@ -17,12 +17,34 @@
 
 package org.agatom.springatom.rest;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.agatom.springatom.rest.processors.InfoPageResourceProcessor;
+import org.agatom.springatom.web.infopages.component.helper.InfoPageLinkHelper;
+import org.agatom.springatom.web.infopages.mapping.InfoPageMappings;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.binding.convert.converters.StringToEnum;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * <small>Class is a part of <b>SpringAtom</b> and was created at 21.03.14</small>
@@ -38,18 +60,76 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguratio
 		useDefaultFilters = false
 )
 public class SARepositoryRestMvcConfiguration extends RepositoryRestMvcConfiguration {
+	private static final Logger                     LOGGER                = Logger.getLogger(SARepositoryRestMvcConfiguration.class);
+	public static final  StringToEnum               STRING_TO_ENUM        = new StringToEnum();
+	@Autowired
+	@Qualifier("restProperties")
+	private              Properties                 restProperties        = null;
+	private              Map<Boolean, List<Object>> enableDisableFeatures = Maps.newHashMap();
 
-	public SARepositoryRestMvcConfiguration() {
-		super();
+	@PostConstruct
+	private void postConstruct() {
+		Assert.notNull(this.restProperties, "restProperties not found [null]");
+		this.populateEnableDisableFeatuers();
+	}
+
+	private void populateEnableDisableFeatuers() {
+		this.populateForProperty("rest.jackson.enableFeatures", true, STRING_TO_ENUM);
+		this.populateForProperty("rest.jackson.disableFeatures", false, STRING_TO_ENUM);
+	}
+
+	private void populateForProperty(final String propProperties, final boolean key, final StringToEnum stringToEnum) {
+		String[] val = this.restProperties.getProperty(propProperties).split(",");
+		List<Object> objects = Lists.newArrayListWithExpectedSize(val.length);
+
+		for (final String prop : val) {
+			final String property = prop.substring(prop.lastIndexOf(".") + 1);
+			try {
+				final Class<?> propertyClazz = this.getPropertyClass(prop);
+				objects.add(stringToEnum.convertSourceToTargetClass(property, propertyClazz));
+			} catch (Exception exp) {
+				LOGGER.error(String.format("Error in resolving enableDisable feature from prop=%s", prop), exp);
+			}
+		}
+		this.enableDisableFeatures.put(key, objects);
+	}
+
+	private Class<?> getPropertyClass(final String prop) throws Exception {
+		if (prop.contains(SerializationFeature.class.getName())) {
+			return SerializationFeature.class;
+		} else if (prop.contains(DeserializationFeature.class.getName())) {
+			return DeserializationFeature.class;
+		}
+		throw new Exception(
+				String.format("%s does not relate to either %s or %s",
+						prop,
+						ClassUtils.getShortName(SerializationFeature.class),
+						ClassUtils.getShortName(DeserializationFeature.class)
+				)
+		);
 	}
 
 	@Override
 	protected void configureJacksonObjectMapper(final ObjectMapper objectMapper) {
 		super.configureJacksonObjectMapper(objectMapper);
 
-		objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-		objectMapper.disable(SerializationFeature.INDENT_OUTPUT);
-
-		objectMapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		for (final Boolean key : this.enableDisableFeatures.keySet()) {
+			final List<Object> objects = this.enableDisableFeatures.get(key);
+			for (final Object obj : objects) {
+				if (key) {
+					if (ClassUtils.isAssignableValue(SerializationFeature.class, obj)) {
+						objectMapper.enable((SerializationFeature) obj);
+					} else {
+						objectMapper.enable((DeserializationFeature) obj);
+					}
+				} else {
+					if (ClassUtils.isAssignableValue(SerializationFeature.class, obj)) {
+						objectMapper.disable((SerializationFeature) obj);
+					} else {
+						objectMapper.disable((DeserializationFeature) obj);
+					}
+				}
+			}
+		}
 	}
 }
