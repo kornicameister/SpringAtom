3d53aae2c80fbb13cb8cd31112f5e7543d8c6daf
kornicameister <kornicameister@gmail.com>
2014-05-28 22:26:48 +0200

    #126
    - added two new exceptions

---
+++ src/main/java/org/agatom/springatom/core/exception/SInvalidArgumentException.java
@@ -0,0 +1,42 @@
+/**************************************************************************************************
+ * This file is part of [SpringAtom] Copyright [kornicameister@gmail.com][2014]                   *
+ *                                                                                                *
+ * [SpringAtom] is free software: you can redistribute it and/or modify                           *
+ * it under the terms of the GNU General Public License as published by                           *
+ * the Free Software Foundation, either version 3 of the License, or                              *
+ * (at your option) any later version.                                                            *
+ *                                                                                                *
+ * [SpringAtom] is distributed in the hope that it will be useful,                                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  *
+ * GNU General Public License for more details.                                                   *
+ *                                                                                                *
+ * You should have received a copy of the GNU General Public License                              *
+ * along with [SpringAtom].  If not, see <http://www.gnu.org/licenses/gpl.html>.                  *
+ **************************************************************************************************/
+
+package org.agatom.springatom.core.exception;
+
+/**
+ * <small>Class is a part of <b>SpringAtom</b> and was created at 17.05.14</small>
+ *
+ * @author kornicameister
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public class SInvalidArgumentException
+		extends SException {
+	private static final long serialVersionUID = -440145598477615431L;
+
+	public SInvalidArgumentException(final String attrName) {
+		super(SInvalidArgumentException.getMessage(attrName));
+	}
+
+	private static String getMessage(final String attrName) {
+		return String.format("Invalid argument %s", attrName);
+	}
+
+	public SInvalidArgumentException(final String attrName, final Throwable cause) {
+		super(SInvalidArgumentException.getMessage(attrName), cause);
+	}
+}
