4fa320d22fb5d16e41b0995667231363a219e35c
kornicameister <kornicameister@gmail.com>
2014-06-15 11:51:51 +0200

    #145
    Code clean, reorganized
    Added builderId as the first param of the data request URL

--- src/main/java/org/agatom/springatom/webmvc/converters/du/converters/ToTableRequestWebConverter.java
+++ src/main/java/org/agatom/springatom/webmvc/converters/du/converters/ToTableRequestWebConverter.java
@@ -60,7 +60,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
  * <small>Class is a part of <b>SpringAtom</b> and was created at 01.06.14</small>
  *
  * @author kornicameister
- * @version 0.0.1
+ * @version 0.0.2
  * @since 0.0.1
  */
 @WebConverter(key = ToTableRequestWebConverter.SELECTOR)
@@ -79,9 +79,86 @@ public class ToTableRequestWebConverter
 	protected Serializable doConvert(final String key, final Object value, final Persistable<?> persistable, final ComponentDataRequest webRequest) throws Exception {
 		LOGGER.trace(String.format("doConverter(key=%s,value=%s)", key, value));
 
+		String builderId = this.getExplicitlyDefinedBuilderId(key, webRequest);
+		if (!StringUtils.hasText(builderId)) {
+			builderId = this.getBuilderIdFromTableDataType(key, persistable);
+		}
+
+		if (StringUtils.hasText(builderId)) {
+			final TableRequest request = new TableRequest();
+
+			request.setLabel(this.getLabel(key, persistable));
+			request.setBuilderId(builderId)
+					.setConfigurationUrl(linkTo(methodOn(SVComponentsDefinitionController.class).onTableConfigRequest(builderId, null)).withRel("configuration"))
+					.setDataUrl(linkTo(methodOn(SVComponentsDataController.class).onTableDataRequest(builderId, null, null)).withSelfRel().withRel("data"))
+					.setData(this.componentContextFactory.buildContext(persistable))
+					.setId(key);
+
+			return request;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Resolves {@code builderId} using {@code key} and {@link org.springframework.data.domain.Persistable}.
+	 * If {@code key} is a property of {@code persistable} therefore {@link javax.persistence.metamodel.EntityType#getAttribute(String)} returns
+	 * valid {@link javax.persistence.metamodel.PluralAttribute} for {@code key}, this method is able to determine {@code builderId} by calling
+	 * {@link org.agatom.springatom.web.component.core.repository.ComponentBuilderRepository#getBuilderId(Class, org.agatom.springatom.web.component.core.builders.ComponentProduces)}
+	 * where:
+	 * <ol>
+	 * <li>{@code class} corresponds to attribute type</li>
+	 * <li>{@link org.agatom.springatom.web.component.core.builders.ComponentProduces} is {@link org.agatom.springatom.web.component.core.builders.ComponentProduces#TABLE_COMPONENT}</li>
+	 * </ol>
+	 *
+	 * <b>Note</b>
+	 * This method will throw {@link org.agatom.springatom.webmvc.converters.du.exception.WebConverterException} if found {@link javax.persistence.metamodel.Attribute} is neither {@link javax.persistence.metamodel.Attribute#isCollection()}
+	 * nor {@link javax.persistence.metamodel.Attribute#isAssociation()}
+	 *
+	 * @param key         property path
+	 * @param persistable persistable to get {@link javax.persistence.metamodel.Attribute} definition for {@code path}
+	 *
+	 * @return builderId
+	 */
+	private String getBuilderIdFromTableDataType(final String key, final Persistable<?> persistable) {
 		String builderId = null;
 
-		// first check if the builder was not defined explicitly
+		final EntityDescriptor<? extends Persistable> descriptor = this.entityDescriptors.getDescriptor(persistable.getClass());
+
+		final Attribute<?, ?> attribute = descriptor.getEntityType().getAttribute(key);
+		if (!attribute.isAssociation() || !attribute.isCollection()) {
+			throw new WebConverterException(String.format("Requested to convert %s as table request, but %s was not recognized as association for %s", key, key, ClassUtils.getUserClass(persistable.getClass())));
+		}
+
+		final PluralAttribute<?, ?, ?> pluralAttribute = (PluralAttribute<?, ?, ?>) attribute;
+		final Class<?> associatedType = ClassUtils.getUserClass(pluralAttribute.getElementType().getJavaType());
+
+		LOGGER.trace(String.format("%s corresponds to %s", key, ClassUtils.getUserClass(associatedType)));
+
+		final boolean hasBuilder = this.builderRepository.hasBuilder(associatedType, ComponentProduces.TABLE_COMPONENT);
+		if (hasBuilder) {
+			builderId = this.builderRepository.getBuilderId(associatedType, ComponentProduces.TABLE_COMPONENT);
+		}
+
+		LOGGER.trace(String.format("%s %s to existing %s builder", associatedType, (hasBuilder ? "corresponds" : "does not corresponds"), ComponentProduces.TABLE_COMPONENT));
+		return builderId;
+	}
+
+	/**
+	 * Tries to locate explicitly defined {@code builderId} if such was set in {@link org.agatom.springatom.web.component.infopages.elements.InfoPageAttributeComponent}
+	 * during compile time for {@link org.agatom.springatom.web.component.infopages.elements.InfoPageComponent} template
+	 *
+	 * @param key        property path
+	 * @param webRequest web request to get {@link org.agatom.springatom.web.component.infopages.elements.InfoPageComponent} from
+	 *
+	 * @return builderId if found or null
+	 *
+	 * @see org.agatom.springatom.web.component.infopages.provider.builder.InfoPageComponentBuilderService
+	 * @see org.agatom.springatom.web.component.infopages.elements.InfoPageAttributeComponent#dynamicProperties
+	 */
+	private String getExplicitlyDefinedBuilderId(final String key, final ComponentDataRequest webRequest) {
+		String builderId = null;
+
 		final ContentComponent<?> component = (ContentComponent<?>) webRequest.getComponent();
 		final Optional<? extends EmbeddableComponent> match = FluentIterable.from(component).firstMatch(new Predicate<EmbeddableComponent>() {
 			@Override
@@ -102,48 +179,7 @@ public class ToTableRequestWebConverter
 			final Map<String, Object> dynamicProperties = forPath.getDynamicProperties();
 			builderId = (String) dynamicProperties.get("builderId");
 		}
-		// first check if the builder was not defined explicitly
-
-		// if not, locate one
-		if (!StringUtils.hasText(builderId)) {
-			final EntityDescriptor<? extends Persistable> descriptor = this.entityDescriptors.getDescriptor(persistable.getClass());
-
-			final Attribute<?, ?> attribute = descriptor.getEntityType().getAttribute(key);
-			if (!attribute.isAssociation() || !attribute.isCollection()) {
-				throw new WebConverterException(String.format("Requested to convert %s as table request, but %s was not recognized as association for %s", key, key, ClassUtils.getUserClass(persistable.getClass())));
-			}
-
-			final PluralAttribute<?, ?, ?> pluralAttribute = (PluralAttribute<?, ?, ?>) attribute;
-			final Class<?> associatedType = ClassUtils.getUserClass(pluralAttribute.getElementType().getJavaType());
-
-			LOGGER.trace(String.format("%s corresponds to %s", key, ClassUtils.getUserClass(associatedType)));
-
-			final boolean hasBuilder = this.builderRepository.hasBuilder(associatedType, ComponentProduces.TABLE_COMPONENT);
-			if (hasBuilder) {
-				builderId = this.builderRepository.getBuilderId(associatedType, ComponentProduces.TABLE_COMPONENT);
-			}
-
-			LOGGER.trace(String.format("%s %s to existing %s builder", associatedType, (hasBuilder ? "corresponds" : "does not corresponds"), ComponentProduces.TABLE_COMPONENT));
-		}
-
-
-		// it is it is still empty, return null
-		// otherwise populate TableRequest
-
-		if (StringUtils.hasText(builderId)) {
-			final TableRequest request = new TableRequest();
-
-			request.setLabel(this.getLabel(key, persistable));
-			request.setBuilderId(builderId)
-					.setConfigurationUrl(linkTo(methodOn(SVComponentsDefinitionController.class).onTableConfigRequest(null, null)).withRel("configuration"))
-					.setDataUrl(linkTo(methodOn(SVComponentsDataController.class).onTableDataRequest(null, null)).withSelfRel().withRel("data"))
-					.setData(this.componentContextFactory.buildContext(persistable))
-					.setId(key);
-
-			return request;
-		}
-
-		return null;
+		return builderId;
 	}
 
 	/**
