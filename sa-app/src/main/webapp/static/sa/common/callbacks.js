00733b050094c3d51d08dbb9ba7a8c6552716f5c
kornicameister <kornicameister@gmail.com>
2015-02-15 00:03:49 +0100

    - fix empty args array after slicing first arg

--- sa-app/src/main/webapp/static/sa/common/callbacks.js
+++ sa-app/src/main/webapp/static/sa/common/callbacks.js
@@ -8,7 +8,7 @@ define(
             cancelEvent: function cancelEvent(func) {
                 var args = _.toArray(arguments).slice(1);
                 func(args);
-                if (args[0].preventDefault) {
+                if (args.length > 0 && args[0].preventDefault) {
                     args[0].preventDefault();
                 }
                 return false;
